c7f75c6be1da2072a6b72f4a2cde02fbeee2d275
Ezhil <ezhildhiraviya@gmail.com>
2025-08-29 15:14:07 +0000

    [verifier] updated

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -2,9 +2,9 @@ use risc0_zkvm::Receipt;
 use serde_json;
 use std::fs;
 fn main() {
-    const METHOD_ID: [u32; 8] =  [4615346, 3088364040, 3309643489, 2403364783, 529424834, 3266678953, 590165670, 1240344216];
+    const METHOD_ID: [u32; 8] =  [8615346, 3088364040, 3309643489, 2403364783, 529424834, 3266678953, 590165670, 1240344216];
 
-    //  Load the proof.json
+    //  deserialize the proof.json
     let data = fs::read_to_string("proof.json")
         .expect("Failed to read proof.json");
 
